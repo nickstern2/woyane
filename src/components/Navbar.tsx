950b8acaaea4a2a4d8fb3b9103f41f84f7142ec7
Nick <nicks@avi.com>
2025-02-07 02:10:15 -0800

    added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,93 +18,147 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useTheme } from "@mui/material/styles";
+import { useAuth } from "../providers/useAuth";
+import { logOut } from "../utils/auth-utils";
+import { RegisterOrLoginModal } from "./RegisterOrLoginModal";
 
-const navItems = ["Home", "About", "Services", "Contact"];
+// const navItems = ["Home", "About", "Services", "Contact"];
+const navItems = [
+  { label: "Home", id: "home-section" },
+  { label: "About", id: "about-section" },
+  { label: "Services", id: "services-section" },
+  { label: "Contact", id: "contact-section" },
+];
 
-const Navbar: React.FC = () => {
+const handleScroll = (id: string) => {
+  const section = document.getElementById(id);
+  if (section) {
+    console.log("!!Scroll");
+    section.scrollIntoView({ behavior: "smooth" });
+    window.history.pushState(null, "", `#${id}`); // Optional: Update URL
+  }
+};
+
+type NavBarProps = {
+  setIsNavModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Navbar: React.FC<NavBarProps> = ({ setIsNavModalOpen }) => {
+  const { user } = useAuth();
+  // const { user, loading, userData, refetchUserData } = useAuth();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [mobileOpen, setMobileOpen] = useState(false);
+  // const [modalOpen, setModalOpen] = useState(false);
+
+  // const handleCloseModal = () => {
+  //   console.log("!!Handle close modal");
+  //   setIsNavModalOpen(false);
+  // };
+
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) =>
     setAnchorEl(event.currentTarget);
   const handleMenuClose = () => setAnchorEl(null);
+  const handleLogout = () => {
+    logOut();
+    // Close toggle
+    handleMenuClose();
+  };
 
-  const toggleDrawer = () => setMobileOpen((prev) => !prev);
+  // const toggleDrawer = () => setModalOpen((prev) => !prev);
 
+  console.log("!user", user);
+  const isUserSignedIn = !!user?.email;
+  const isUserVerified = user?.emailVerified ?? false;
+  const userNeedsVerification = isUserSignedIn && !isUserVerified;
   return (
-    <AppBar
-      id='react-navbar'
-      className='react-navbar'
-      position='relative'
-      sx={{
-        background: "#000000",
-        boxShadow: "none",
-      }}>
-      <Toolbar
+    <>
+      <AppBar
+        id='react-navbar'
+        className='react-navbar'
+        position='relative'
         sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
+          background: "#000000",
+          boxShadow: "none",
         }}>
-        {/* Logo or Brand */}
-        <Typography variant='h6' component='div' sx={{ fontWeight: "bold" }}>
-          Woyane
-        </Typography>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+          {/* Logo or Brand */}
+          <Typography variant='h6' component='div' sx={{ fontWeight: "bold" }}>
+            Woyane
+          </Typography>
 
-        {/* Desktop Navigation */}
-        {!isMobile && (
-          <Box sx={{ display: "flex", gap: 1, ml: "auto" }}>
-            {navItems.map((item, index) => (
+          {/* Desktop Navigation */}
+          {!isMobile && (
+            <Box sx={{ display: "flex", gap: 1, ml: "auto" }}>
+              {navItems.map(({ label, id }, index) => (
+                <Button
+                  key={id}
+                  color='inherit'
+                  sx={{ mr: index === navItems.length - 1 ? 2 : 0 }} // Extra padding for the last item
+                  onClick={() => handleScroll(id)}>
+                  {label}
+                </Button>
+              ))}
+            </Box>
+          )}
+
+          {/* //TODO: think top half is broken  */}
+          {!isMobile &&
+            (isUserSignedIn ? (
+              <IconButton onClick={handleMenuOpen} sx={{ p: 0 }}>
+                {/* <IconButton onClick={handleMenuOpen} sx={{ p: 0 }}> */}
+                <Avatar alt='Profile' src='/profile.jpg' />
+              </IconButton>
+            ) : (
               <Button
-                key={item}
-                color='inherit'
-                sx={{ mr: index === navItems.length - 1 ? 2 : 0 }} // Extra padding for the last item
-              >
-                {item}
+                variant='contained'
+                onClick={() => {
+                  console.log("!!open nav modal ");
+                  setIsNavModalOpen(true);
+                }}
+                sx={{ mt: 2 }}>
+                {"Log In"}
               </Button>
             ))}
-          </Box>
-        )}
 
-        {!isMobile && (
-          <IconButton onClick={handleMenuOpen} sx={{ p: 0 }}>
-            <Avatar alt='Profile' src='/profile.jpg' />
-          </IconButton>
-        )}
+          {/* Profile Dropdown Menu */}
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+            sx={{ mt: 1 }}>
+            <MenuItem onClick={handleMenuClose}>Purchase History</MenuItem>
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
 
-        {/* Profile Dropdown Menu */}
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-          sx={{ mt: 1 }}>
-          <MenuItem onClick={handleMenuClose}>Purchase History</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
-        </Menu>
+          {/* Mobile Menu Button */}
+          {isMobile && (
+            <IconButton edge='start' color='inherit' onClick={handleMenuOpen}>
+              <MenuIcon />
+            </IconButton>
+          )}
+        </Toolbar>
 
-        {/* Mobile Menu Button */}
-        {isMobile && (
-          <IconButton edge='start' color='inherit' onClick={toggleDrawer}>
-            <MenuIcon />
-          </IconButton>
-        )}
-      </Toolbar>
-
-      {/* Mobile Drawer */}
-      <Drawer anchor='left' open={mobileOpen} onClose={toggleDrawer}>
-        <List sx={{ width: 250 }}>
-          {navItems.map((item) => (
+        {/* Mobile Drawer */}
+        {/* <Drawer anchor='left' open={mobileOpen} onClose={handleMenuOpen}>
+          <List sx={{ width: 250 }}>
+            {navItems.map((item) => (
             <ListItem key={item} disablePadding>
               <ListItemButton onClick={toggleDrawer}>
                 <ListItemText primary={item} />
               </ListItemButton>
             </ListItem>
           ))}
-        </List>
-      </Drawer>
-    </AppBar>
+          </List>
+        </Drawer> */}
+      </AppBar>
+    </>
   );
 };
 
