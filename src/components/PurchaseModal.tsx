3a80cb58446a9a28a7e3889b2485a9608e43ef47
Nick <nicks@avi.com>
2025-02-07 02:10:35 -0800

    refactor purchase modal

--- src/components/PurchaseModal.tsx
+++ src/components/PurchaseModal.tsx
@@ -14,11 +14,18 @@ import {
   TextField,
   FormLabel,
   Input,
+  Tooltip,
+  IconButton,
 } from "@mui/material";
-import { Formik, Field, Form, useFormik } from "formik";
+import { Formik, Field, Form, useFormik, FormikConfig } from "formik";
 import { auth } from "../firebase"; // Import Firebase
 import { PurchaseModalValidationSchema } from "../utils/validations";
-import { signIn, signUp } from "../utils/auth-utils";
+import {
+  AuthStateDisabledMessage,
+  signIn,
+  signUp,
+  UserAuthState,
+} from "../utils/auth-utils";
 import {
   getAuth,
   onAuthStateChanged,
@@ -26,6 +33,13 @@ import {
   User,
 } from "firebase/auth";
 import { ConfirmationDialog } from "./ConfirmationDialog";
+import RegisterOrLoginForm from "../forms/RegisterOrLoginForm/RegisterOrLoginForm";
+import { LoginFormInitialValuesType } from "../utils/types";
+import ReusableModal from "./ReusableModal";
+import { LoginFormInitialValues } from "../utils/consts";
+import { handleLoginRegisterFormSubmit } from "../forms/RegisterOrLoginForm/registe-or-login-utils";
+import LockIcon from "@mui/icons-material/Lock";
+import { summaryStyles } from "../utils/styles";
 
 const InputStyles = {
   // width: "50%",
@@ -56,44 +70,31 @@ const AccordionDetailsStyles = {
 
 interface PurchaseModalProps {
   open: boolean;
+  loginErrors: boolean;
+  user: User | null;
   handleClose: (reason?: "backdropClick" | "escapeKeyDown") => void;
   handlePurchase: (type: "rent" | "buy") => void;
-  userNeedsVerification: boolean;
-  isUserVerified: boolean;
-  isUserSignedIn: boolean;
-  user: User | null;
   refetchUserData: (user: User) => Promise<void>;
+  setLoginErrors: React.Dispatch<React.SetStateAction<boolean>>;
+  authState: UserAuthState;
 }
-
-type InitialValuesType = {
-  email: string;
-  password: string;
-  billingAddress: string;
-  cardDetails: string;
-};
 
 const handleAccordionToggle = (
   setter: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
   setter((prev) => !prev);
 };
-const InitialValues: InitialValuesType = {
-  email: "",
-  password: "",
-  billingAddress: "",
-  cardDetails: "",
-};
+
 const BillingInitialValues = {
   billingAddress: "",
   cardDetails: "",
 };
-
 const PurchaseModal: React.FC<PurchaseModalProps> = ({
   open,
-  isUserSignedIn,
-  isUserVerified,
+  authState,
   user,
-  userNeedsVerification,
+  loginErrors,
+  setLoginErrors,
   refetchUserData,
   handleClose,
   handlePurchase,
@@ -105,8 +106,9 @@ const PurchaseModal: React.FC<PurchaseModalProps> = ({
 
   const [emailVerified, setEmailVerified] = useState(false);
   const [isSignUpSectionExpanded, setIsSignUpSectionExpanded] = useState(true);
-  const [isPaymentSectionExpanded, setIsPaymentSectionExpanded] =
-    useState(false);
+  const [isPaymentSectionExpanded, setIsPaymentSectionExpanded] = useState(
+    authState === UserAuthState.VERIFIED
+  );
   const [userData, setUserData] = useState<User | null>(null);
   const [emailVerifiedTest, setEmailVerifiedTest] = useState(false);
 
@@ -133,9 +135,6 @@ const PurchaseModal: React.FC<PurchaseModalProps> = ({
 
     setIntervalId(interval); // Save the interval ID for cleanup
   };
-
-  console.log("!userNeedsVerification", userNeedsVerification);
-  console.log("!userDataTest", userData);
 
   // TODO: this work after modal is closed
   // useEffect(() => {
@@ -165,97 +164,49 @@ const PurchaseModal: React.FC<PurchaseModalProps> = ({
   //   return () => unsubscribe(); // Cleanup the listener on unmount
   // }, [open]);
 
-  // const handleSubmit = async (values: any, { setSubmitting }: any) => {
-  //   console.log("!!Submit", values);
-  //   setSubmitting(true);
-  //   try {
-  //     // Create a new user
-  //     // TODO: If fail try to signIn instead, depending on failure message maybe
-  //     const userCredential = await signUp(values.email, values.password);
-  //     const user = userCredential.user;
+  const getTooltipMessage = (authState: UserAuthState) => {
+    let message = "";
+    switch (authState) {
+      case UserAuthState.NOT_SIGNED_IN:
+        message = AuthStateDisabledMessage.NOT_SIGNED_IN;
+        break;
 
-  //     // Send verification email
-  //     await sendEmailVerification(user);
+      case UserAuthState.SIGNED_IN_NOT_VERIFIED:
+        message = AuthStateDisabledMessage.SIGNED_IN_NOT_VERIFIED;
+        break;
 
-  //     //
-  //     console.log("!*Call startPollingForEmailVerification");
-  //     startPollingForEmailVerification(user);
-  //     // TODO: Toast notification
-  //     console.log("Verification email sent to", values.email);
-  //   } catch (error: any) {
-  //     console.error("Error signing up:", error?.message);
-  //   }
-  // };
-  const handleSubmit = async (
-    values: { email: string; password: string },
-    { setSubmitting }: any
-  ) => {
-    console.log("!!Submit", values);
-    setSubmitting(true);
-    try {
-      // Attempt to sign up
-      const userCredential = await signUp(values.email, values.password);
-      const user = userCredential.user;
-
-      // Send verification email
-      await sendEmailVerification(user);
-      console.log("!*Call startPollingForEmailVerification");
-      // startPollingForEmailVerification(user);
-
-      // TODO: Toast notification
-      console.log("Verification email sent to", values.email);
-    } catch (error: any) {
-      console.error("Error signing up:", error?.message);
-
-      // If email is already in use, try signing in instead
-      if (error.code === "auth/email-already-in-use") {
-        console.log("User already exists. Attempting to sign in...");
-        try {
-          console.log("!Attempting to sign in");
-          const signInCredential = await signIn(values.email, values.password);
-          console.log("Successfully signed in:", signInCredential.user);
-        } catch (signInError: any) {
-          console.error("Error signing in:", signInError?.message);
-        }
-      }
-    } finally {
-      setSubmitting(false);
+      default:
+        message = "";
+        break;
     }
+    return message;
   };
-  const formik = useFormik({
-    initialValues: InitialValues,
+  const tooltipDisabledMessage = getTooltipMessage(authState);
+
+  const loginFormUseFormikValues: FormikConfig<LoginFormInitialValuesType> = {
+    initialValues: LoginFormInitialValues,
     validationSchema: PurchaseModalValidationSchema,
-    // TODO: Isolate logic to reuse on both forms
-    onSubmit: handleSubmit,
-  });
+    onSubmit: async (values, formikHelpers) => {
+      await handleLoginRegisterFormSubmit(
+        values,
+        formikHelpers,
+        setLoginErrors
+        // handleClose
+      );
+    },
+  };
+
   const paymentInfoFormik = useFormik({
     initialValues: BillingInitialValues,
     // validationSchema: PurchaseModalValidationSchema,
     // TODO: Isolate logic to reuse on both forms
     onSubmit: async (values, { setSubmitting }) => {
       console.log("!!Submit", values);
-      // setSubmitting(true);
-      // try {
-      //   // Create a new user
-      //   // TODO: If fail try to signIn instead. depending on failure message maybe
-      //   const userCredential = await signUp(values.email, values.password);
-      //   const user = userCredential.user;
-
-      //   // Send verification email
-      //   await sendEmailVerification(user);
-
-      //   //
-      //   console.log("!*Call startPollingForEmailVerification");
-      //   startPollingForEmailVerification(user);
-      //   // TODO: Toast notification
-      //   console.log("Verification email sent to", values.email);
-      // } catch (error: any) {
-      //   console.error("Error signing up:", error?.message);
-      // }
     },
   });
 
-  console.log("!!isUserVerified", isUserVerified);
+  console.log("!!authState", authState);
+
   useEffect(() => {
     // Cleanup polling interval when the modal is closed or component unmounts
     return () => {
@@ -264,99 +215,69 @@ const PurchaseModal: React.FC<PurchaseModalProps> = ({
       }
     };
   }, [intervalId]); // Only run cleanup when intervalId changes
-
+  const onSubmit = (formikOnSubmit: () => void) => {
+    formikOnSubmit();
+  };
   return (
-    <>
-      <Dialog
-        open={open}
-        // onClose={handleClose}
-        maxWidth='md'
-        fullWidth
-        onClose={(event, reason) => handleClose(reason)}>
-        <DialogTitle>Modal Title</DialogTitle>
-
-        <form onSubmit={formik.handleSubmit} id='woyane-ud'>
-          {!isUserVerified && (
-            <Accordion
-              expanded={isSignUpSectionExpanded}
-              onChange={() => handleAccordionToggle(setIsSignUpSectionExpanded)}
-              style={AccordionStyles}>
-              {/* If user is not signed in, show them signup */}
-              {/* //TODO: Figure out a way to rework this if i cant signin and signup in the same attempt*/}
-              {/* Sign up form */}
-              {!isUserSignedIn ? (
-                <>
-                  <AccordionSummary>
-                    <Typography>Register</Typography>
-                  </AccordionSummary>
-
-                  <AccordionDetails style={AccordionDetailsStyles}>
-                    <Stack
-                      display='flex'
-                      flexDirection='column'
-                      justifyContent='center'
-                      spacing={2}
-                      sx={{ padding: 2, boxShadow: 3 }}>
-                      <TextField
-                        required
-                        name='email'
-                        label='Email'
-                        fullWidth
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        error={
-                          formik.touched.email && Boolean(formik.errors.email)
-                        }
-                        helperText={formik.touched.email && formik.errors.email}
-                      />
-                      <TextField
-                        required
-                        name='password'
-                        id='password'
-                        type='password'
-                        label='Password'
-                        fullWidth
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        error={
-                          formik.touched.password &&
-                          Boolean(formik.errors.password)
-                        }
-                        helperText={
-                          formik.touched.password && formik.errors.password
-                        }
-                      />
-                      <Button type='submit' variant='contained' sx={{ mt: 2 }}>
-                        Register
-                      </Button>
-                    </Stack>
-                  </AccordionDetails>
-                </>
-              ) : (
-                <AccordionDetails style={AccordionDetailsStyles}>
-                  {/* //TODO: style and add success picture maybe  */}
-                  <div>
-                    Please accept email verification
-                    <br />
-                    If you are seeing this and already have accepted email
-                    verification, please refresh your browser
-                  </div>
-                </AccordionDetails>
-              )}
-            </Accordion>
-          )}
-        </form>
+    <ReusableModal
+      loginErrors={loginErrors}
+      title='Purchase or Rent Woyane'
+      open={open}
+      handleClose={handleClose}
+      customOnSubmit={() => paymentInfoFormik.submitForm()}
+      tooltipDisabledMessage={tooltipDisabledMessage}
+      authState={authState}>
+      <>
+        <RegisterOrLoginForm
+          formId='woyane-ud'
+          isAccordion
+          authState={authState}
+          formikInitializerValues={loginFormUseFormikValues}
+          expanded={isSignUpSectionExpanded}
+          onToggle={() => handleAccordionToggle(setIsSignUpSectionExpanded)}
+          displaySignedInConfirmation={
+            authState === UserAuthState.SIGNED_IN_NOT_VERIFIED
+          } //TODO: determine
+          UserIsSignedInConfirmationComponent={
+            <AccordionDetails style={AccordionDetailsStyles}>
+              <div>
+                Please accept email verification
+                <br />
+                If you are seeing this and already have accepted email
+                verification, please refresh your browser
+              </div>
+            </AccordionDetails>
+          } //TODO: Design this for when authState === UserAuthState.SIGNED_IN_NOT_VERIFIED
+        />
 
         <form id='woyane-bd' onSubmit={paymentInfoFormik.handleSubmit}>
-          {/* //TODO: change this to && after testing */}
           <Accordion
-            expanded={userNeedsVerification || isPaymentSectionExpanded}
+            expanded={
+              authState !== UserAuthState.VERIFIED
+                ? false
+                : isPaymentSectionExpanded
+            }
             onChange={() => handleAccordionToggle(setIsPaymentSectionExpanded)}
             style={AccordionStyles}>
-            <AccordionSummary>
-              <Typography>Payment Information</Typography>
+            <AccordionSummary sx={summaryStyles}>
+              {authState !== UserAuthState.VERIFIED ? (
+                <Tooltip title={tooltipDisabledMessage}>
+                  <span>
+                    <IconButton
+                      size='small'
+                      disableRipple
+                      sx={{
+                        pointerEvents: "none", // Prevents clicking or interaction
+                        cursor: "default", // Ensures no hand cursor on hover
+                        backgroundColor: "transparent", // Optional: Removes background hover effect
+                        "&:hover": { backgroundColor: "transparent" }, // Prevents hover styling
+                      }}>
+                      <LockIcon color='disabled' />
+                    </IconButton>
+                  </span>
+                </Tooltip>
+              ) : null}
+              <Typography alignSelf='center'>Payment Information</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <Stack
@@ -389,33 +310,8 @@ const PurchaseModal: React.FC<PurchaseModalProps> = ({
             </AccordionDetails>
           </Accordion>
         </form>
-
-        <DialogActions>
-          <Stack direction='row' padding='16px' spacing={2}>
-            <Button
-              type='submit'
-              onClick={() => paymentInfoFormik.submitForm()}
-              // onClick={() => handlePurchase("buy")}
-              variant='contained'
-              color='primary'>
-              Confirm
-            </Button>
-            {/* //TODO:  Add confirmation dialog */}
-            <Button
-              onClick={() => setShowConfirmDialog(true)}
-              // onClick={() => handleClose()}
-              color='inherit'>
-              Cancel
-            </Button>
-          </Stack>
-        </DialogActions>
-      </Dialog>
-      <ConfirmationDialog
-        open={showConfirmDialog}
-        onClose={() => setShowConfirmDialog(false)}
-        onConfirm={handleClose}
-      />
-    </>
+      </>
+    </ReusableModal>
   );
 };
 
