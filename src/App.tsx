153842936ad3b03e4bcab2368807a4775bd2a27f
Nick <nicks@avi.com>
2025-02-07 02:11:19 -0800

    added toast and added navModal controls

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import Hero from "./components/Hero";
 import Auth from "./components/Auth";
 import Navbar from "./components/Navbar";
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const [isNavModalOpen, setIsNavModalOpen] = React.useState(false);
+  console.log("!!* isNavModalOpen main", isNavModalOpen);
   return (
     <>
       <CssBaseline />
       {/* <Auth /> */}
+      <ToastContainer position='top-right' autoClose={3000} />
 
-      <Navbar />
-      <Hero />
+      <Navbar setIsNavModalOpen={setIsNavModalOpen} />
+      <Hero
+        isNavModalOpen={isNavModalOpen}
+        setIsNavModalOpen={setIsNavModalOpen}
+      />
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./hooks/useAuth";
+import { AuthProvider } from "./providers/useAuth";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+// import StripeCheckoutForm from './StripeCheckoutForm';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
