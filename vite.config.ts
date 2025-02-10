62c6a67072eaf6659105b4a9a31934248c0b8ff6
Nick <nicks@avi.com>
2025-02-10 11:17:33 -0800

    added emptyOutDir to prevent vite from clearing dist when build
    Was having an issue where build populates and then clears dist so this command ensures it stays while 'rm -rf dist' was added to the prefix of build to clear before the process not after

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
-      },
-    },
+    emptyOutDir: false,
   },
 });
