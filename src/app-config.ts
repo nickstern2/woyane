ddffad2dc57d407f3b095607849fbd45dcb26c43
Nick <nicks@avi.com>
2025-02-10 00:43:09 -0800

    added production vs dev keys

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,10 +1,15 @@
 export type Config = {
   FirebaseProjectId: string;
+  StripePK: string;
 };
 
 const loadConfig = (): Config => {
   return {
     FirebaseProjectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? "",
+    StripePK:
+      import.meta.env.MODE === "production"
+        ? import.meta.env.VITE_STRIPE_LIVE_PK ?? ""
+        : import.meta.env.VITE_STRIPE_TEST_PK ?? "",
   };
 };
 
