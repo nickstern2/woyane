d173fc5a498f61f239bb604e0cd2645708cfbd7e
Nick <nicks@avi.com>
2025-02-10 18:03:09 -0800

    added MUI createTheme to remove accordion border
    Will add the rest of styles into createTheme

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,39 @@ import Navbar from "./components/Navbar";
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiAccordion: {
+      styleOverrides: {
+        root: {
+          "&::before": {
+            display: "none",
+            content: "none",
+          },
+        },
+      },
+    },
+  },
+});
 
 const App = () => {
   const [isNavModalOpen, setIsNavModalOpen] = React.useState(false);
   console.log("!!* isNavModalOpen main", isNavModalOpen);
   return (
     <>
-      <CssBaseline />
-      {/* <Auth /> */}
-      <ToastContainer position='top-right' autoClose={3000} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {/* <Auth /> */}
+        <ToastContainer position='top-right' autoClose={3000} />
 
-      <Navbar setIsNavModalOpen={setIsNavModalOpen} />
-      <Hero
-        isNavModalOpen={isNavModalOpen}
-        setIsNavModalOpen={setIsNavModalOpen}
-      />
+        <Navbar setIsNavModalOpen={setIsNavModalOpen} />
+        <Hero
+          isNavModalOpen={isNavModalOpen}
+          setIsNavModalOpen={setIsNavModalOpen}
+        />
+      </ThemeProvider>
     </>
   );
 };
