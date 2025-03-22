de8356d01a6451017f6aa19475630bcd3be31f67
Nick <nicks@avi.com>
2025-03-22 14:26:35 -0400

    Added forgotPassword functionality

--- src/forms/RegisterOrLoginForm/RegisterOrLoginForm.tsx
+++ src/forms/RegisterOrLoginForm/RegisterOrLoginForm.tsx
@@ -7,6 +7,7 @@ import {
   Typography,
   TextField,
   Button,
+  Box,
 } from "@mui/material";
 import { FormikConfig, FormikProps, FormikValues, useFormik } from "formik";
 import { AccordionStyles, linkStyles, summaryStyles } from "../../utils/styles";
@@ -17,13 +18,16 @@ import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { sendEmailVerification } from "firebase/auth";
 import {
+  forgotPassword,
   PurchaseType,
+  resendEmailVerification,
   signIn,
   signUp,
   UserAuthState,
 } from "../../utils/auth-utils";
 import LockIcon from "@mui/icons-material/Lock";
 import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+import { ForgotPasswordValidationSchema } from "../../utils/validations";
 
 interface PaymentFormProps {
   formikInitializerValues: FormikConfig<LoginFormInitialValuesType>;
@@ -42,9 +46,10 @@ const RegisterOrLoginForm: React.FC<PaymentFormProps> = ({
   authState,
   onToggle,
 }) => {
-  console.log("!authState", authState);
   const formik = useFormik(formikInitializerValues);
   const [isLogin, setIsLogin] = useState(true);
+  const [isForgotPassword, setIsForgotPassword] = useState(false);
+  const handleForgotPassword = () => setIsForgotPassword((prev) => !prev);
 
   // Toggle Login/Register Mode
   const toggleAuthMode = useCallback(
@@ -63,12 +68,17 @@ const RegisterOrLoginForm: React.FC<PaymentFormProps> = ({
       isLogin={isLogin}
       toggleAuthMode={toggleAuthMode}
       authState={authState}>
-      <RegisterOrLoginFields
-        formik={formik}
-        formId={formId}
-        isLogin={isLogin}
-        isAccordion={isAccordion}
-      />
+      {isForgotPassword ? (
+        <ForgotPasswordFields isAccordion={isAccordion} />
+      ) : (
+        <RegisterOrLoginFields
+          formik={formik}
+          formId={formId}
+          isLogin={isLogin}
+          isAccordion={isAccordion}
+          handleForgotPassword={handleForgotPassword}
+        />
+      )}
     </ContainerWrapper>
   );
 };
@@ -143,12 +153,20 @@ const SubmissionStatus: React.FC<{ authState: UserAuthState }> = ({
       )}
 
       {authState === UserAuthState.SIGNED_IN_NOT_VERIFIED && (
-        <>
-          <CheckCircleIcon color='success' />
-          <Typography>
-            Sign up successful. Check email for confirmation.
+        <Stack direction='column' spacing={1}>
+          <Stack direction='row' spacing={1}>
+            <CheckCircleIcon color='success' />
+            <Typography>
+              Sign up successful. Check email for confirmation.
+            </Typography>
+          </Stack>
+          <Typography
+            alignSelf='center'
+            onClick={() => resendEmailVerification()}
+            sx={linkStyles}>
+            Resend verification email
           </Typography>
-        </>
+        </Stack>
       )}
     </Stack>
   );
@@ -159,46 +177,179 @@ const RegisterOrLoginFields: React.FC<{
   formId: string;
   isLogin: boolean;
   isAccordion: boolean;
-}> = ({ formik, formId, isLogin, isAccordion }) => (
-  <form onSubmit={formik.handleSubmit} id={formId}>
-    <Stack
-      display='flex'
-      flexDirection='column'
-      justifyContent='center'
-      spacing={2}
-      sx={{ padding: 2, ...(isAccordion ? { boxShadow: 3 } : {}) }}>
-      <TextField
-        required
-        name='email'
-        label='Email'
-        size='small'
-        fullWidth
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        error={formik.touched.email && Boolean(formik.errors.email)}
-        helperText={formik.touched.email && formik.errors.email}
-      />
-      <TextField
-        required
-        name='password'
-        type='password'
-        label='Password'
-        size='small'
-        fullWidth
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        error={formik.touched.password && Boolean(formik.errors.password)}
-        helperText={formik.touched.password && formik.errors.password}
-      />
-      <Button
-        disabled={formik.isSubmitting}
-        type='submit'
-        variant='contained'
-        sx={{ mt: 2 }}>
-        {isLogin ? "Sign In" : "Sign Up"}
-      </Button>
-    </Stack>
-  </form>
-);
+  handleForgotPassword: () => void;
+}> = ({ formik, formId, isLogin, isAccordion, handleForgotPassword }) => {
+  return (
+    <form onSubmit={formik.handleSubmit} id={formId} noValidate>
+      <Stack
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        spacing={2}
+        sx={{ padding: 2, ...(isAccordion ? { boxShadow: 3 } : {}) }}>
+        <TextField
+          required
+          name='email'
+          label='Email'
+          size='small'
+          fullWidth
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          required
+          name='password'
+          type='password'
+          label='Password'
+          size='small'
+          fullWidth
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+
+        <Typography
+          onClick={handleForgotPassword}
+          component='span'
+          sx={linkStyles}>
+          {"Forgot your password?"}
+        </Typography>
+
+        {/* {isForgotPassword ? <ForgotPasswordFields /> : null} */}
+
+        <Button
+          disabled={formik.isSubmitting}
+          type='submit'
+          variant='contained'
+          sx={{ mt: 2 }}>
+          {isLogin ? "Sign In" : "Sign Up"}
+        </Button>
+      </Stack>
+    </form>
+  );
+};
+
+const ForgotPasswordFields: React.FC<{ isAccordion: boolean }> = ({
+  isAccordion,
+}) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: ForgotPasswordValidationSchema,
+    onSubmit: async (values, { setSubmitting }) => {
+      console.log("!submit forgot password values", values);
+      // TODO: IF SUCCESSFUL, CLOSE MODAL
+      forgotPassword(values.email);
+    },
+  });
+  return (
+    <>
+      <form id='forgot-password-form' onSubmit={formik.handleSubmit} noValidate>
+        <Stack
+          display='flex'
+          flexDirection='column'
+          justifyContent='center'
+          spacing={2}
+          sx={{ padding: 2, ...(isAccordion ? { boxShadow: 3 } : {}) }}>
+          <TextField
+            required
+            name='email'
+            label='Email'
+            size='small'
+            fullWidth
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+          <Button
+            disabled={formik.isSubmitting}
+            type='submit'
+            variant='contained'
+            sx={{ mt: 2 }}>
+            {"Send Password Reset Link"}
+          </Button>
+        </Stack>
+      </form>
+    </>
+  );
+};
+
+const forgotPasswordText =
+  "Enter your user account's verified email address and we will send you a password reset link.";
+
+// const RegisterOrLoginFields2: React.FC<{
+//   formik: FormikProps<LoginFormInitialValuesType>;
+//   formId: string;
+//   isLogin: boolean;
+//   isAccordion: boolean;
+// }> = ({ formik, formId, isLogin, isAccordion }) => {
+//   const [isForgotPassword, setIsForgotPassword] = useState(false);
+//   const handleForgotPassword = () => setIsForgotPassword((prev) => !prev);
+
+//   return (
+//     <form onSubmit={formik.handleSubmit} id={formId} noValidate>
+//       <Stack
+//         display='flex'
+//         flexDirection='column'
+//         justifyContent='center'
+//         spacing={2}
+//         sx={{ padding: 2, ...(isAccordion ? { boxShadow: 3 } : {}) }}>
+//         {!isForgotPassword ? (
+//           <>
+//             <TextField
+//               required
+//               name='email'
+//               label='Email'
+//               size='small'
+//               fullWidth
+//               value={formik.values.email}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               error={formik.touched.email && Boolean(formik.errors.email)}
+//               helperText={formik.touched.email && formik.errors.email}
+//             />
+//             <TextField
+//               required
+//               name='password'
+//               type='password'
+//               label='Password'
+//               size='small'
+//               fullWidth
+//               value={formik.values.password}
+//               onChange={formik.handleChange}
+//               onBlur={formik.handleBlur}
+//               error={formik.touched.password && Boolean(formik.errors.password)}
+//               helperText={formik.touched.password && formik.errors.password}
+//             />
+//           </>
+//         ) : null}
+//         {isForgotPassword ? null : (
+//           <Typography
+//             onClick={handleForgotPassword}
+//             component='span'
+//             sx={linkStyles}>
+//             {"Forgot your password?"}
+//           </Typography>
+//         )}
+//         {isForgotPassword ? <ForgotPasswordFields /> : null}
+//         {isForgotPassword ? null : (
+//           <Button
+//             disabled={formik.isSubmitting}
+//             type='submit'
+//             variant='contained'
+//             sx={{ mt: 2 }}>
+//             {isLogin ? "Sign In" : "Sign Up"}
+//           </Button>
+//         )}
+//       </Stack>
+//     </form>
+//   );
+// };
